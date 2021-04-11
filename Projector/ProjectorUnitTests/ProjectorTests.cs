479589d33bb469ef813b067b88bbae28d2b61fc2
BENNETT Paul <paul.bennett@hexagon.com>
2021-04-11 16:54:32 +0100

    2nd constructor test passing

--- Projector/ProjectorUnitTests/ProjectorTests.cs
+++ Projector/ProjectorUnitTests/ProjectorTests.cs
@@ -27,14 +27,16 @@ namespace ProjectorUnitTests
         {
             Vector3d mockedNormal = _mockCreationHelper.Create<Vector3d>();
             _mockCreationHelper.ArrangeStatic(() => Vector3d.Dot(mockedNormal, Arg.Matches<Vector3d>(x => x.X == 0 && x.Y == 0 && x.Z == 1))).Returns(0);
-            mockedNormal.Arrange(x => x.X).Returns(1);
+            mockedNormal.Arrange(x => x.X).Returns(0);
             mockedNormal.Arrange(x => x.Y).Returns(0);
-            mockedNormal.Arrange(x => x.Z).Returns(0);
+            mockedNormal.Arrange(x => x.Z).Returns(1);
 
             Point3d mockedPoint = _mockCreationHelper.Create<Point3d>();
             mockedPoint.Arrange(x => x.X).Returns(-56);
             mockedPoint.Arrange(x => x.Y).Returns(32);
             mockedPoint.Arrange(x => x.Z).Returns(122);
+
+            //only have to mock point and normal because I don't know how to mock an operator.
 
             double distance = 50;
             Projector.Projector p = new Projector.Projector(mockedPoint, distance,mockedNormal);
@@ -48,6 +50,43 @@ namespace ProjectorUnitTests
             Assert.AreEqual(p.V2.Y, 0);
             Assert.AreEqual(p.V2.Z, 0);
 
+            _mockCreationHelper.AssertAll();
+
+        }
+
+        [TestMethod]
+        public void ConstructorTest2()
+        {
+            Vector3d mockedNormal = _mockCreationHelper.Create<Vector3d>();
+            mockedNormal.Arrange(x => x.X).Returns(6 / Math.Sqrt(85));
+            mockedNormal.Arrange(x => x.Y).Returns(-7 / Math.Sqrt(85));
+            mockedNormal.Arrange(x => x.Z).Returns(0);
+
+            Vector3d vCross2 = new Vector3d(-0.759256602, -0.650791373, 0);
+            Vector3d vCross1 = new Vector3d(0, 0, -1);
+            Vector3d vCross2Normalised = new Vector3d(-0.759256602, -0.650791373, 0);
+            Vector3d vCross1Normalised = new Vector3d(0, 0, -1);
+
+            Point3d mockedPoint = _mockCreationHelper.Create<Point3d>();
+            mockedPoint.Arrange(x => x.X).Returns(-56);
+            mockedPoint.Arrange(x => x.Y).Returns(32);
+            mockedPoint.Arrange(x => x.Z).Returns(122);
+
+            //only have to mock point and normal because I don't know how to mock an operator.
+
+            _mockCreationHelper.ArrangeStatic(() => Vector3d.Dot(mockedNormal, Arg.Matches<Vector3d>(x => x.X == 0 && x.Y == 0 && x.Z == 1))).Returns(1);
+            _mockCreationHelper.ArrangeStatic(() => Vector3d.CrossProduct(mockedNormal, Arg.Matches<Vector3d>(x => x.X == 0 && x.Y == 0 && x.Z == 1))).Returns(vCross2);
+            _mockCreationHelper.ArrangeStatic(() => Vector3d.CrossProduct(vCross2Normalised, mockedNormal)).Returns(vCross1);
+
+            _mockCreationHelper.ArrangeStatic(() => Vector3d.Normalise(vCross1)).Returns(vCross1Normalised);
+            _mockCreationHelper.ArrangeStatic(() => Vector3d.Normalise(vCross2)).Returns(vCross2Normalised);
+
+            double distance = 50;
+
+            Projector.Projector p = new Projector.Projector(mockedPoint, distance, mockedNormal);
+
+            Assert.AreSame(vCross1Normalised, p.V1);
+            Assert.AreSame(vCross2Normalised, p.V2);
             _mockCreationHelper.AssertAll();
 
         }
