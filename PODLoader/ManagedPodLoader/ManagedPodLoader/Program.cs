59a15d9a75c58991c8b2dc13f7c62cb366b8265e
BENNETT Paul <paul.bennett@hexagon.com>
2021-06-17 09:54:52 +0100

    ManagedPODLoader

---
+++ PODLoader/ManagedPodLoader/ManagedPodLoader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedPodLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadedPod lp = new LoadedPod(@"D:\IRUXGit\bin\Debug\I-Configure.exe.manifest", @"D:\PODPCFStore\AttributePopper\POP\10-Y2-LS-1002.POD");
+
+
+
+        }
+    }
+}
