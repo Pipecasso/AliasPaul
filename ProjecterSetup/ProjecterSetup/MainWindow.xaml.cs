349feb32ed27fa7e44b819a92c291f1287f04776
BENNETT Paul <paul.bennett@hexagon.com>
2021-08-23 17:50:42 +0100

    Projector Setup

---
+++ ProjecterSetup/ProjecterSetup/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ManagedPodLoader;
+using Microsoft.Win32;
+
+namespace ProjecterSetup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string _manfiest = @"D:\AliasPaul\bin\Debug\I-Configure.exe.manifest";
+        private LoadedPod loadedPod;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void LoadPod_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = "c:\\";
+            ofd.Filter = "pod files (*.pod)|*.pod";
+            ofd.RestoreDirectory = true;
+            bool? yes = ofd.ShowDialog();
+            if (yes.HasValue && yes==true)
+            {
+                string mypod = ofd.FileName;
+                loadedPod = new LoadedPod(_manfiest, mypod);
+                loaded_pod.Text = mypod;
+            }
+
+
+        }
+    }
+}
